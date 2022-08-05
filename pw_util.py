3943b04a5d74136ae965bc6fbef5577e2059f6df
SasiVatsal <71326149+sasivatsal7122@users.noreply.github.com>
2022-08-05 22:02:36 +0530

    changed pw_util

--- pw_util.py
+++ pw_util.py
@@ -4,30 +4,37 @@ import streamlit_authenticator as stauth
 
 
 
-names=["Sasi Vatsal","Likhith","Lokesh"]
-usernames = ["sasivatsal7122@gmail.com","likhithbavisetti@gmail.com",'lokeshwarlakhi@gmail.com']
-passwords=["7382117167","9391377120",'7989104871']
-
-
-hashed_passwords=stauth.Hasher(passwords).generate()
-
-file_path=Path(__file__).parent/"pkl_creds/pw.pkl"
-with file_path.open("wb")as file:
-    pickle.dump(hashed_passwords,file)
+def auth_driver(club,names_ls,usernames_ls,passwords_ls,designation_ls):
+    hashed_passwords=stauth.Hasher(passwords_ls).generate()
+    file_path=Path(__file__).parent/f"pkl_creds/{club}_pw.pkl"
+    with file_path.open("wb")as file:
+        pickle.dump(hashed_passwords,file)
+        
+    file_path=Path(__file__).parent/f"pkl_creds/{club}_usernames.pkl"
+    with file_path.open("wb")as file:
+        pickle.dump(usernames_ls,file)
+        
+    file_path=Path(__file__).parent/f"pkl_creds/{club}_names.pkl"
+    with file_path.open("wb")as file:
+        pickle.dump(names_ls,file)
+        
+    designation_ls = dict(zip(names_ls,designation_ls))
     
-file_path=Path(__file__).parent/"pkl_creds/usernames.pkl"
-with file_path.open("wb")as file:
-    pickle.dump(usernames,file)
-    
-file_path=Path(__file__).parent/"pkl_creds/names.pkl"
-with file_path.open("wb")as file:
-    pickle.dump(names,file)
-    
-designation = {'Chief Secretary','Vice Secretary','Associate Secretary'}
-designation = dict(zip(names,designation))
+    file_path=Path(__file__).parent/f"pkl_creds/{club}_designation.pkl"
+    with file_path.open("wb")as file:
+        pickle.dump(designation_ls,file)
 
-print(designation)
 
-file_path=Path(__file__).parent/"pkl_creds/designation.pkl"
-with file_path.open("wb")as file:
-    pickle.dump(designation,file)
+owasp_names=["Sasi Vatsal","Likhith","Lokesh"]
+owasp_usernames = ["sasivatsal7122@gmail.com","likhithbavisetti@gmail.com",'lokeshwarlakhi@gmail.com']
+owasp_passwords=["7382117167","9391377120",'7989104871']
+owasp_designation = ['Chief Secretary','Vice Secretary','Associate Secretary']
+
+vigniters_names=["Sasi Vatsal"]
+vigniters_usernames = ["sasivatsal7122@gmail.com"]
+vigniters_passwords=["7382117167"]
+vigniters_designation = ['Chief Igniter']
+
+if __name__=='__main__':
+    auth_driver(club='OWASP_VIIT',names_ls=owasp_names,usernames_ls=owasp_usernames,passwords_ls=owasp_passwords,designation_ls=owasp_designation)
+    auth_driver(club='VIGNITERS',names_ls=vigniters_names,usernames_ls=vigniters_usernames,passwords_ls=vigniters_passwords,designation_ls=vigniters_designation)
